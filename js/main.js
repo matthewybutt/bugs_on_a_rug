f0cf4c3bcf077987e466d4f04341bfece1e1e7c7
Matthew Butt <matthew.y.butt@gmail.com>
2015-10-13 18:17:18 -0700

    Change How to Play button from alert to pop up div

--- js/main.js
+++ js/main.js
@@ -51,13 +51,28 @@ function restartGame(){
 
 ///////////////////////////////////////////////////////////////////////////
 //"How to Play" Button
-var howTo = document.getElementById("howTo")
+//var howTo = document.getElementById("howTo")
+
+//howTo.addEventListener('click', howToPlay);
+
+//function howToPlay (){
+//	alert("Bugs On A Rug is a matching game where players try and clear the bugs off of the rug! On your turn click the color selector button and match the color of the bug given to you to clear it off the rug. If you can't make a match you get a stink bug, collect 3 stink bugs and you lose!");
+//}
+var howToMsg = document.getElementById('howToMessage')
 
 howTo.addEventListener('click', howToPlay);
 
-function howToPlay (){
-	alert("Bugs On A Rug is a matching game where players try and clear the bugs off of the rug! On your turn click the color selector button and match the color of the bug given to you to clear it off the rug. If you can't make a match you get a stink bug, collect 3 stink bugs and you lose!");
+function howToPlay(){	
+	howToMsg.style.display = "block";
 }
+
+var howToOutBtn = document.getElementById('howToOutBtn')
+
+howToOutBtn.addEventListener('click', howToOutBtnClick);
+
+function howToOutBtnClick (){
+	howToMsg.style.display = "none";
+} 
 
 ///////////////////////////////////////////////////////////////////////////
 //"Random Color Selector" Button
